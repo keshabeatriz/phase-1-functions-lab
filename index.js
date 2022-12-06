9c3f2c3ec43e6a032b3ccccac71c09843bb4b634
Kesha <kesha.marges@gmail.com>
2022-12-06 16:08:11 -0500

    commit tree a086d58e33a28f84941d78498768500fa33a20d4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street){
+    distanceFromHqInBlocks(street);
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const blocksTraveled = Math.abs(start - destination);
+  return blocksTraveled * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = Math.abs(start - destination) *264;  
+    let fare;
+    
+    if (feet < 400) {
+      fare = 0;
+    } else if (feet >= 400 && feet <= 2000) {
+      fare = (feet - 400) * 0.02;
+    } else if (feet > 2000 && feet <= 2500) {
+      fare = 25;
+    } else if (feet > 2500) {
+      fare = 'cannot travel that far';
+    }
+    return fare;
+  }
